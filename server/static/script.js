22f49d2c334d0fefa058379376d44ccd2732c73a
Dmitriy Govorukhin <dmitriy.govorukhin@gmail.com>
2018-01-31 00:40:39 +0300

    log in text area

--- server/static/script.js
+++ server/static/script.js
@@ -5,15 +5,16 @@ var janus = null;
 var streaming = null;
 var started = false;
 
-$(document).ready(function() {
+function log(text) {
     var l = document.getElementById('log');
+
+    l.value += (text + '\n');
+    l.scrollTop = l.scrollHeight;
+}
+
+$(document).ready(function() {
     var button = document.getElementById('btn');
-
-    function log(text) {
-        l.value += (text + '\n');
-        l.scrollTop = l.scrollHeight;
-    }
-
+    
    /* var wsport = 8091;
 
     var url1 = 'ws://' + document.location.hostname + ':' + wsport + '/';*/
@@ -133,30 +134,36 @@ $(document).ready(function() {
 });
 
 function startJanus(){
-    console.log("starting Janus");
+    log("starting Janus");
+
     $('#btn').click(function() {
         if(started){
             return;
         }
+
         started = true;
+
         // Make sure the browser supports WebRTC
         if(!Janus.isWebrtcSupported()) {
             console.error("No webrtc support");
+
             return;
         }
         // Create session
         janus = new Janus({
             server: server,
             success: function() {
-                console.log("Success");
+                log("Success");
+
                 attachToStreamingPlugin(janus);
             },
             error: function(error) {
-                console.log(error);
-                console.log("janus error");
+                log(error);
+
+                log("janus error");
             },
             destroyed: function() {
-                console.log("destroyed");
+                log("destroyed");
             }
         });
     });
@@ -164,32 +171,37 @@ function startJanus(){
 
 function attachToStreamingPlugin(janus){
     // Attach to streaming plugin
-    console.log("Attach to streaming plugin");
+    log("Attach to streaming plugin");
     janus.attach({
         plugin: "janus.plugin.streaming",
         success: function(pluginHandle) {
             streaming = pluginHandle;
-            console.log("Plugin attached! (" + streaming.getPlugin() + ", id=" + streaming.getId() + ")");
+            log("Plugin attached! (" + streaming.getPlugin() + ", id=" + streaming.getId() + ")");
             // Setup streaming session
             updateStreamsList();
         },
         error: function(error) {
-            console.log("  -- Error attaching plugin... " + error);
+            log("  -- Error attaching plugin... " + error);
             console.error("Error attaching plugin... " + error);
         },
         onmessage: function(msg, jsep) {
-            console.log(" ::: Got a message :::");
-            console.log(JSON.stringify(msg));
+            log(" ::: Got a message :::");
+
+            log(JSON.stringify(msg));
+
             processMessage(msg);
+
             handleSDP(jsep);
         },
         onremotestream: function(stream) {
-            console.log(" ::: Got a remote stream :::");
-            console.log(JSON.stringify(stream));
+            log(" ::: Got a remote stream :::");
+
+            log(JSON.stringify(stream));
+
             handleStream(stream);
         },
         oncleanup: function() {
-            console.log(" ::: Got a cleanup notification :::");
+            log(" ::: Got a cleanup notification :::");
         }
     });//end of janus.attach
 }
@@ -200,65 +212,84 @@ function processMessage(msg){
         var status = result["status"];
         switch(status) {
             case 'starting':
-                console.log("starting - please wait...");
+                log("starting - please wait...");
+
                 break;
             case 'preparing':
-                console.log("preparing");
+                log("preparing");
+
                 break;
             case 'started':
-                console.log("started");
+                log("started");
+
                 break;
             case 'stopped':
-                console.log("stopped");
+                log("stopped");
+
                 stopStream();
+
                 break;
         }
     }else{
-        console.log("no status available");
+        log("no status available");
     }
 }
 // we never appear to get this jsep thing
 function handleSDP(jsep){
-    console.log(" :: jsep :: ");
-    console.log(jsep);
+    log(" :: jsep :: ");
+
+    log(jsep);
+
     if(jsep !== undefined && jsep !== null) {
-        console.log("Handling SDP as well...");
-        console.log(jsep);
+        log("Handling SDP as well...");
+
+        log(jsep);
+
         // Answer
         streaming.createAnswer({
             jsep: jsep,
             media: { audioSend: false, videoSend: false },      // We want recvonly audio/video
             success: function(jsep) {
-                console.log("Got SDP!");
-                console.log(jsep);
+                log("Got SDP!");
+
+                log(jsep);
+
                 var body = { "request": "start" };
+
                 streaming.send({"message": body, "jsep": jsep});
             },
             error: function(error) {
-                console.log("WebRTC error:");
-                console.log(error);
+                log("WebRTC error:");
+
+                log(error);
+
                 console.error("WebRTC error... " + JSON.stringify(error));
             }
         });
     }else{
-        console.log("no sdp");
+        log("no sdp");
     }
 }
 function handleStream(stream){
-    console.log(" ::: Got a remote stream :::");
-    console.log(JSON.stringify(stream));
+    log(" ::: Got a remote stream :::");
+
+    log(JSON.stringify(stream));
+
     // Show the stream and hide the spinner when we get a playing event
-    console.log("attaching remote media stream");
+    log("attaching remote media stream");
+
     Janus.attachMediaStream($('#big-video').get(0), stream);
 
     $("#big-video").bind("playing", function () {
-        console.log("got playing event");
+        log("got playing event");
     });
 }
 
 function updateStreamsList() {
     var body = { "request": "list" };
-    console.log("Sending message (" + JSON.stringify(body) + ")");
+
+    log("Sending message (" + JSON.stringify(body) + ")");
+
     streaming.send({"message": body, success: function(result) {
             if(result === null || result === undefined) {
                 console.error("no streams available");
@@ -266,10 +297,14 @@ function updateStreamsList() {
             }
             if(result["list"] !== undefined && result["list"] !== null) {
                 var list = result["list"];
-                console.log("Got a list of available streams:");
-                console.log(list);
-                console.log("taking the first available stream");
+                log("Got a list of available streams:");
+
+                log(list);
+
+                log("taking the first available stream");
+
                 var theFirstStream = list[0];
+
                 startStream(theFirstStream);
             }else{
                 console.error("no streams available - list is null");
@@ -279,19 +314,26 @@ function updateStreamsList() {
 }
 function startStream(selectedStream) {
     var selectedStreamId = selectedStream["id"];
-    console.log("Selected video id #" + selectedStreamId);
+
+    log("Selected video id #" + selectedStreamId);
+
     if(selectedStreamId === undefined || selectedStreamId === null) {
-        console.log("No selected stream");
+        log("No selected stream");
+
         return;
     }
     var body = { "request": "watch", id: parseInt(selectedStreamId) };
+
     streaming.send({"message": body});
 }
 
 function stopStream() {
-    console.log("stopping stream");
+    log("stopping stream");
+
     var body = { "request": "stop" };
+
     streaming.send({"message": body});
+
     streaming.hangup();
 }
 
